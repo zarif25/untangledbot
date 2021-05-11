7171b0c481ad7edce428c42c29b05467344ab092
Zarif Muhtasim <2502.zarif@gmail.com>
2021-05-11 23:51:56 +0600

    updated key

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ async def info():
     channel = client.get_channel(841727986351865896)
     await channel.send()
 
-client.run("ODQxNzI1NzIyMTQ1NTg3Mjgw.YJq8Kg.FlkGjHYymiFqDakSXMoePM4oLkQ")
+client.run("ODQxNzI1NzIyMTQ1NTg3Mjgw.YJq8Kg.g_gp-Je5L2CsmMz49cD9X3g6FPA")
