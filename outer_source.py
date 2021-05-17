423bf003c4e1c5ae0a108d1e5efbe6154e27cd50
Zarif Muhtasim <2502.zarif@gmail.com>
2021-05-17 10:26:28 +0600

    print to log

--- outer_source.py
+++ outer_source.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from xml.sax import saxutils as su
 from urllib.parse import quote
+from logger import log_info
 
 
 def match_accuracy(hash, title):
@@ -10,17 +11,18 @@ def match_accuracy(hash, title):
 
 
 def get_source_link(src, title):
-    src = src.split(',')[-1]
+    src = src.split(',')[-1].strip(' ')
     query = quote(title + " " + src)
-    res = requests.get(f"https://news.google.com/rss/search?q={query}+when:1d")
+    res = requests.get(f"https://news.google.com/rss/search?q={query}")
     soup = BeautifulSoup(su.unescape(res.text), 'lxml')
     try:
         first_a_tag = soup.item.a
     except Exception as e:
         raise Exception("Problem parsing rss feed", e)
     rss_title = first_a_tag.text
-    print(
-        f"\nRSS TITLE: {rss_title}\nTITLE: {title}\n{match_accuracy(title, rss_title)}\n")
+    log_info("RSS TITLE", rss_title)
+    log_info("TITLE", {title})
+    log_info("match accuracy", match_accuracy(title, rss_title))
     rss_url = first_a_tag['href']
     if match_accuracy(title, rss_title) > 0.70:
         return rss_url
