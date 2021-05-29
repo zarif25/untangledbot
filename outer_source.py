04b06a4c9200c12ebe72e15ae24d0732bdc500a1
Zarif Muhtasim <2502.zarif@gmail.com>
2021-05-29 11:48:28 +0600

    moved: similarity_between func to utils

--- outer_source.py
+++ outer_source.py
@@ -1,13 +1,8 @@
-from difflib import SequenceMatcher
+from utils import similarity_between
 from bs4 import BeautifulSoup
 import requests
 from xml.sax import saxutils as su
-# from urllib.parse import quote
 from logger import log_info
-
-
-def match_accuracy(hash, title):
-    return SequenceMatcher(None, hash, title).ratio()
 
 
 def get_source_link(src, title):
@@ -21,7 +16,7 @@ def get_source_link(src, title):
     rss_title = first_a_tag.text
     log_info("RSS TITLE", rss_title)
     log_info("TITLE", title)
-    log_info("match accuracy", match_accuracy(title, rss_title))
+    log_info("match accuracy", similarity_between(title, rss_title))
     rss_url = first_a_tag['href']
-    if match_accuracy(title, rss_title) > 0.70:
+    if similarity_between(title, rss_title) > 0.70:
         return rss_url
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 from time import localtime
+from difflib import SequenceMatcher
 
 def get_theme():
     t_hour = (localtime().tm_hour + 6) % 24
     return 'light' if 3 <= t_hour <= 18 else 'dark'
+
+def similarity_between(hash, title):
+    return round(SequenceMatcher(None, hash, title).ratio(), 2)
