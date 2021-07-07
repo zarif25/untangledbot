54385fafcdce8ef61870fe5160ed13971694acdf
Zarif25 <2502.zarif@gmail.com>
2021-07-07 15:42:19 +0600

    added: doc

--- providers/utils/bn2en.py
+++ providers/utils/bn2en.py
@@ -1,30 +1,37 @@
+"""A Bangla to English translator
+
+This module can translate bangla digits, numbers, months and
+dates to English
+"""
+
 from datetime import datetime
 BN_DIGITS = "০১২৩৪৫৬৭৮৯"
+BN_MONTHS = {
+    "জানুয়ারি": 1,
+    "ফেব্রুয়ারী": 2,
+    "মার্চ": 3,
+    "এপ্রিল": 4,
+    "মে": 5,
+    "জুন": 6,
+    "জুলাই": 7,
+    "আগষ্ট": 8,
+    "সেপ্টেম্বর": 9,
+    "অক্টোবর": 10,
+    "নভেম্বর": 11,
+    "ডিসেম্বর": 12,
+}
 
 
-def to_en_digit(digit: str):
+def to_en_digit(digit: str) -> int:
     return BN_DIGITS.find(digit)
 
 
-def to_en_num(num: str):
+def to_en_num(num: str) -> int:
     return int(''.join(str(to_en_digit(digit)) for digit in num))
 
 
-def to_en_month(month: str):
-    return {
-        "জানুয়ারি": 1,
-        "ফেব্রুয়ারী": 2,
-        "মার্চ": 3,
-        "এপ্রিল": 4,
-        "মে": 5,
-        "জুন": 6,
-        "জুলাই": 7,
-        "আগষ্ট": 8,
-        "সেপ্টেম্বর": 9,
-        "অক্টোবর": 10,
-        "নভেম্বর": 11,
-        "ডিসেম্বর": 12,
-    }[month]
+def to_en_month(month: str) -> int:
+    return BN_MONTHS[month]
 
 
 def to_en_datetime(dtime: str, format="dd mmmm, yyyy") -> datetime:
